96a8a67db53e79f5db4b07da9e85381bff00d549
Chimerakraken <johnnyrufinojr@gmail.com>
2021-09-04 09:31:21 +0800

    fabricjs

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -7,12 +7,44 @@
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/460/fabric.min.js" integrity="sha512-ybPp3kCrNQXdvTfh99YLIdnajWnQvHuEKDJ+b26mxI9w+pLhnBo3HmNLJ1pEUBFO1Q0bfJxApeqecNbmaV763g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <!-- Styles -->
   <title>Sap1</title>
 </head>
 
-<body class="antialiased font-sans overflow-hidden">
-asdasd
+<body class="antialiased font-sans">
+  <main>
+    <div class="w-full" align="center" style="margin-top: 10vh;">
+      <div class="grid grid-cols-4 gap-x-4 mx-56">
+        <div class="col-span-2 h-full bg-gray-500"></div>
+        <div class="col-span-2 h-full">
+          <canvas id="c"  width="800" height="800"  style="border: 1px solid black;"></canvas>
+        </div>
+      </div>
+    </div>
+  </main>
 </body>
 
+<script>
+  $(document).ready(function(){
+    var canvas = new fabric.Canvas('c');
+    canvas.width  = $(window).width()*2; 
+    canvas.height = 1000;
+
+  // create a rectangle object
+    var rect = new fabric.Rect({
+      left: 75,
+      top: 75,
+      fill: 'red',
+      width: 100,
+      height: 20
+    });
+
+    // "add" rectangle onto canvas
+    canvas.add(rect);
+      })
+</script>
+
 </html>
+
